5d90222dcdc36f5f3e0f53bd2ba8e17bfc2ccecd
Naaz <36826926+Naaz781@users.noreply.github.com>
2021-02-05 16:14:02 +0700

    Add files via upload

---
+++ Fxc7/donasi.js
@@ -0,0 +1,16 @@
+const donasi = (Ig, name) => { 
+	return `       
+Bantu donasi agar bot bisa terus berjalan.\n\n اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ\n_“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
+╭══───┤ DONATE ├───══╮ 
+│
+├ *DONASI BISA MELALUI :*
+├ *PULSA : 083890700901*
+├ *DANA : 083890700901*
+├ *TERIMA KASIH BANYAK YANG SUDAH MAU DONASI*
+│
+╰══──────────────══╯
+
+`
+}
+
+exports.donasi = donasi
---
+++ Fxc7/help.js
@@ -0,0 +1,300 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { 
+	return `
+	
+╔══❍〘 *USER INFO* 〙❍══╗
+├─❍ NAME : *${pushname2}*
+├─❍ LIMIT : *${limitt}*
+├─❍ VERIVICATION : ✅
+├─❍ USER BOT : *${user.length}*
+╰──────────────────────
+
+
+╔══❍〘 *INFO BOT* 〙❍══╗
+├─❍ NAME : *${name}*
+├─❍ TANGGAL : *${tanggal}*
+├─❍ JAM : *${jam}*
+├─❍ *BOT Aktif Tergantung Owner*
+╰──────────────────────
+
+
+[!] _Apa itu prefix?_
+Awalan atau prefiks adalah sebuah afiks yang dibubuhkan pada awal sebuah kata dasar
+
+
+╭◪ *RULESS*
+├❍ *Spam : Auto Block!*
+├❍ *Call : Auto Block!*
+├❍ *Setidaknya Beri Jeda 5 Detik!*
+├❍ *Bug/Error Harap Chat Owner!*
+├❍ *Fitur sebagian dibutuhkan Apikey!*
+├❍ *Gunakan Bot Dengan Bijak!*
+└────────────────────
+
+╭◪ *LIST MENU*
+├❍ Perintah/Command : 「 ${prefix} 」
+├❍ Harap Jangan menSpam BOT.
+├❍ Inget beri jeda 5 Detik.
+└────────────────────
+
+
+╭◪ *ABOUT*
+├❍ *${prefix}report lapor bug*
+├❍ *${prefix}info*
+├❍ *${prefix}donasi*
+├❍ *${prefix}owner*
+├❍ *${prefix}speed*
+├❍ *${prefix}daftar*
+├❍ *${prefix}totaluser*
+├❍ *${prefix}grouplist*
+├❍ *${prefix}blocklist*
+├❍ *${prefix}banlist*
+├❍ *${prefix}premiumlist*
+├❍ *${prefix}bahasa*
+└────────────────────
+╭◪ *MEDIA*
+├❍ *${prefix}tiktokstalk username*
+├❍ *${prefix}igstalk username*
+├❍ *${prefix}insta Link*
+├❍ *${prefix}instastory username*
+├❍ *${prefix}ssweb url*
+├❍ *${prefix}url2img Url*
+├❍ *${prefix}tiktok*
+├❍ *${prefix}fototiktok*
+├❍ *${prefix}meme*
+├❍ *${prefix}memeindo*
+├❍ *${prefix}kbbi*
+├❍ *${prefix}wait*
+├❍ *${prefix}trendtwit*
+├❍ *${prefix}google berita terkini*
+└────────────────────
+╭◪ *CREATOR*
+├❍ *${prefix}quotemaker tx/wtrmk/tema*
+├❍ *${prefix}nulis nama/kelas/text*
+├❍ *${prefix}rain reply image*
+├❍ *${prefix}trigger reply image*
+├❍ *${prefix}rip reply image*
+├❍ *${prefix}wasted reply image*
+├❍ *${prefix}cphlogo [teks1|teks2]*
+├❍ *${prefix}cglitch [teks1|teks2]*
+├❍ *${prefix}cpubg [teks1|teks2]*
+├❍ *${prefix}cml [Text]*
+├❍ *${prefix}tahta [Text]*
+├❍ *${prefix}croman [Text] dan [Text]*
+├❍ *${prefix}cthunder [Text]*
+├❍ *${prefix}cbpink [Text]*
+├❍ *${prefix}cmwolf [Text]*
+├❍ *${prefix}csky [Text]*
+├❍ *${prefix}cwooden [Text]*
+├❍ *${prefix}cflower [Text]*
+├❍ *${prefix}clove [Text]*
+├❍ *${prefix}ccrossfire [Text]*
+├❍ *${prefix}cnaruto [Text]*
+├❍ *${prefix}cparty [Text]*
+├❍ *${prefix}cshadow [Text]*
+├❍ *${prefix}cminion [Text]*
+├❍ *${prefix}cneon [Text]*
+├❍ *${prefix}cneon2 [Text]*
+├❍ *${prefix}cneongreen [Text][Text]*
+├❍ *${prefix}c3d [Text]*
+├❍ *${prefix}csky [Text]*
+├❍ *${prefix}tts id [Text]*
+├❍ *${prefix}ttp [Text]*
+├❍ *${prefix}cballon [Text]*
+├❍ *${prefix}cpaper [Text]*
+├❍ *${prefix}slide [Text]*
+├❍ *${prefix}stiker*
+├❍ *${prefix}gifstiker*
+├❍ *${prefix}toimg*
+├❍ *${prefix}img2url*
+├❍ *${prefix}nobg*
+├❍ *${prefix}tomp3*
+├❍ *${prefix}ocr*
+└────────────────────
+╭◪ *GROUP*
+├❍ *${prefix}modeanime On/Off*
+├❍ *${prefix}welcome On/Off*
+├❍ *${prefix}grup buka/tutup*
+├❍ *${prefix}antilink on/off*
+├❍ *${prefix}naruto*
+├❍ *${prefix}minato*
+├❍ *${prefix}boruto*
+├❍ *${prefix}hinata*
+├❍ *${prefix}sakura*
+├❍ *${prefix}sasuke*
+├❍ *${prefix}kaneki*
+├❍ *${prefix}toukachan*
+├❍ *${prefix}rize*
+├❍ *${prefix}akira*
+├❍ *${prefix}itori*
+├❍ *${prefix}kurumi*
+├❍ *${prefix}miku*
+├❍ *${prefix}anime*
+├❍ *${prefix}animecry*
+├❍ *${prefix}neonime*
+├❍ *${prefix}animekiss*
+├❍ *${prefix}wink*
+├❍ *${prefix}ownergrup*
+├❍ *${prefix}setpp*
+├❍ *${prefix}infogc*
+├❍ *${prefix}add*
+├❍ *${prefix}kick*
+├❍ *${prefix}promote*
+├❍ *${prefix}demote*
+├❍ *${prefix}setname*
+├❍ *${prefix}setdesc*
+├❍ *${prefix}linkgrup*
+├❍ *${prefix}tagme*
+├❍ *${prefix}hidetag*
+├❍ *${prefix}tagall*
+├❍ *${prefix}mentionall*
+├❍ *${prefix}fitnah*
+├❍ *${prefix}listadmin*
+├❍ *${prefix}openanime*
+├❍ *${prefix}edotense*
+└────────────────────
+╭◪ *NSFSW*
+├❍ *${prefix}nsfw On/Off*
+├❍ *${prefix}nsfwloli*
+├❍ *${prefix}nsfwblowjob*
+├❍ *${prefix}nsfwneko*
+├❍ *${prefix}nsfwtrap*
+├❍ *${prefix}hentai*
+├❍ *${prefix}simih On/Off*
+├❍ *${prefix}cerpen*
+├❍ *${prefix}cersex*
+├❍ *${prefix}xxx [Text]*
+├❍ *${prefix}pornhub [Text]*
+└────────────────────
+╭◪ *FUN*
+├❍ *${prefix}anjing*
+├❍ *${prefix}kucing*
+├❍ *${prefix}testime*
+├❍ *${prefix}hilih*
+├❍ *${prefix}say*
+├❍ *${prefix}apakah*
+├❍ *${prefix}kapankah*
+├❍ *${prefix}bisakah*
+├❍ *${prefix}rate*
+├❍ *${prefix}watak*
+├❍ *${prefix}hobby*
+├❍ *${prefix}infogempa*
+├❍ *${prefix}infonomor*
+├❍ *${prefix}quotes*
+├❍ *${prefix}truth*
+├❍ *${prefix}dare*
+├❍ *${prefix}katabijak*
+├❍ *${prefix}fakta*
+├❍ *${prefix}darkjokes*
+├❍ *${prefix}bucin*
+├❍ *${prefix}pantun*
+├❍ *${prefix}katacinta*
+├❍ *${prefix}jadwaltvnow*
+├❍ *${prefix}hekerbucin*
+├❍ *${prefix}katailham*
+├❍ *${prefix}animewp*
+├❍ *${prefix}jarak [Daerah1/Daerah2]*
+├❍ *${prefix}translate en/Apa kabar?*
+├❍ *${prefix}pasangan [Namamu/Pasanganmu]*
+├❍ *${prefix}gantengcek [teks]*
+├❍ *${prefix}cantikcek [teks]*
+├❍ *${prefix}artinama [teks]*
+├❍ *${prefix}persengay [teks]*
+├❍ *${prefix}pbucin [teks]*
+├❍ *${prefix}bpfont [teks]*
+├❍ *${prefix}textstyle [teks]*
+├❍ *${prefix}jadwaltv antv*
+├❍ *${prefix}lirik melukis senja*
+├❍ *${prefix}chord Melukis senja*
+├❍ *${prefix}wiki Adolf Hitler*
+├❍ *${prefix}brainly pertanyaan*
+├❍ *${prefix}resepmasakan rawon*
+├❍ *${prefix}map [daerah]*
+├❍ *${prefix}film Fast and Farious*
+├❍ *${prefix}pinterest gambar kucing*
+├❍ *${prefix}infocuaca [daerah]*
+├❍ *${prefix}jamdunia [daerah]*
+├❍ *${prefix}mimpi Ular*
+├❍ *${prefix}infoalamat [teks]*
+├❍ *${prefix}playstore [apk]*
+└────────────────────
+╭◪ *GAME*
+├❍ *${prefix}asupan*
+├❍ *${prefix}tebakgambar*
+├❍ *${prefix}caklontong*
+├❍ *${prefix}family100*
+├❍ *${prefix}kalkulator 13*12*
+├❍ *${prefix}wp gunung*
+├❍ *${prefix}moddroid lightroom*
+├❍ *${prefix}happymod lightroom*
+└────────────────────
+╭◪ *PRAY*
+├❍ *${prefix}jadwalsholat [daerah]*
+├❍ *${prefix}quran*
+├❍ *${prefix}quransurah 1*
+├❍ *${prefix}tafsir kafir*
+└────────────────────
+╭◪ *OTHER*
+├❍ *${prefix}becrypt string*
+├❍ *${prefix}encode64 string*
+├❍ *${prefix}decode64 encrypt*
+├❍ *${prefix}encode32 string*
+├❍ *${prefix}decode32 encrypt*
+├❍ *${prefix}encbinary string*
+├❍ *${prefix}decbinary encrypt*
+├❍ *${prefix}encoctal string*
+├❍ *${prefix}decoctal encrypt*
+├❍ *${prefix}hashidentifier Encrypt Hash*
+├❍ *${prefix}dorking dork*
+├❍ *${prefix}pastebin teks*
+├❍ *${prefix}tinyurl link*
+├❍ *${prefix}bitly link*
+
+╭◪ *ISENG*
+├❍ *${prefix}spamcall 083xxxxxxxxx*
+├❍ *${prefix}spamsms 083xxxxxxxx/jumlah*
+├❍ *${prefix}spamgmail nazxxxx@gmail.com*
+└────────────────────
+╭◪ *OWNER*
+├❍ *${prefix}addprem mentioned*
+├❍ *${prefix}removeprem mention*
+├❍ *${prefix}setmemlimit*
+├❍ *${prefix}setreply*
+├❍ *${prefix}setprefix*
+├❍ *${prefix}setnamebot*
+├❍ *${prefix}setppbot*
+├❍ *${prefix}bc*
+├❍ *${prefix}bcgc*
+├❍ *${prefix}ban*
+├❍ *${prefix}unban*
+├❍ *${prefix}block*
+├❍ *${prefix}unblock*
+├❍ *${prefix}clearall*
+├❍ *${prefix}delete*
+├❍ *${prefix}clone*
+├❍ *${prefix}getses*
+├❍ *${prefix}leave*
+└────────────────────
+╭◪ *PREMIUM*
+├❍ *${prefix}playmp3 menepi*
+├❍ *${prefix}fb link video*
+├❍ *${prefix}snack link snack video*
+├❍ *${prefix}ytmp3 link yt*
+├❍ *${prefix}ytmp4 link yt*
+├❍ *${prefix}joox Monolog Pamungkas*
+├❍ *${prefix}smule Link Video Smule*
+└────────────────────
+
+`
+}
+
+exports.help = help
+
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
